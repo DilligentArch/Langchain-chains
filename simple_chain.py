d71f3a8eeee11b8e3ea44756add474db5c62e5ad
Nayeb Ahmed Qureshi <nayebqureshi4@gmail.com>
2025-09-24 16:15:37 +0600

    simple chain

---
+++ simple_chain.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+load_dotenv()
+
+prompt= PromptTemplate(
+    template='generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model=ChatOpenAI()
+parser= StrOutputParser()
+
+chain=prompt | model | parser
+
+result=chain.invoke({'topic': 'Cricket'})
+
+print(result)
+#visualizing the chain
+chain.get_graph().print_ascii()
